d3d5a718b718e56b25bcd0ac74d214f1a25e9cc8
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2024-12-26 20:08:22 +0000

    chore: move api key reading to environment variable

--- tests/SolScanner.Tests.Unit/FreeApiTests.cs
+++ tests/SolScanner.Tests.Unit/FreeApiTests.cs
@@ -1,4 +1,5 @@
-﻿using SolScanner;
+﻿using Microsoft.Extensions.Configuration;
+using SolScanner;
 
 namespace Solscanner.Tests.Unit;
 
@@ -9,7 +10,13 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        _client = new SolscanClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE3MzQ5ODM4NTA0NDEsImVtYWlsIjoic29sc2Nhbl85aW54b25AZW1hbGlhcy5hcHAiLCJhY3Rpb24iOiJ0b2tlbi1hcGkiLCJhcGlWZXJzaW9uIjoidjIiLCJpYXQiOjE3MzQ5ODM4NTB9.Gktw1EgpIJO0Ba0FNDKI2YZyjrZ_cMzn3G4Mf2ZgzM8");
+        var config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+
+        var apiKey = config["SolScan:ApiKey"];
+        _client = new SolscanClient(apiKey);
     }
 
     [Test]
